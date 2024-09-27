11974b87211b34dc7aff64e48ae8b6772b110e81
Robert Gajewski <robert.gajewski@gmail.com>
2024-09-27 21:05:36 +0200

    adding a case with a checkbox list

---
+++ S01_selectors_and_locators/tests/L09_locator_checkbox_lists/locator-lists.spec.ts
@@ -0,0 +1,80 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Locator lists", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/practice/simple-multiple-elements-no-ids.html");
+  });
+
+  test("All checkbox on page", async ({ page }) => {
+    /* Arrange: */
+    const elementRole = "checkbox";
+    const expectedElementsCount = 5;
+
+    /* we can define the locator for the element */
+    const checkboxLocator = page.getByRole(elementRole);
+
+    /* print the count of buttons on the page */
+    console.log("number of button elements:", await checkboxLocator.count());
+
+    /* Assert: */
+    /* check if number of buttons is 5 */
+    await expect(checkboxLocator).toHaveCount(expectedElementsCount);
+  });
+
+  test("action on nth checkbox", async ({ page }) => {
+    /* Arrange: */
+    const elementRole = "checkbox";
+    const resultsTestId = "dti-results";
+    const expectedMessage = "Checkbox is checked! (Opt 3!)";
+
+    const buttonLocator = page.getByRole(elementRole);
+    const resultsLocator = page.getByTestId(resultsTestId);
+
+    /* print the count of checkboxes on the page */
+    console.log("number of checkbox elements:", await buttonLocator.count());
+
+    /* Act: */
+    // click on the 3rd button (we count from 0)
+    await buttonLocator.nth(2).click();
+
+    /* display the text content of the results element */
+    // console.info("results text content:", await resultsLocator.textContent());
+
+    /* Assert: */
+    await expect(resultsLocator).toHaveText(expectedMessage);
+  });
+
+  test("action on multiple checkboxes", async ({ page }) => {
+    /* Arrange: */
+    const elementRole = "checkbox";
+    const resultsTestId = "dti-results";
+    const expectedMessage1 = "Checkbox is checked! (Opt 5!)";
+    const expectedMessage2 = "Checkbox is unchecked! (Opt 5!)";
+
+    const checkboxLocator = page.getByRole(elementRole);
+    const resultsLocator = page.getByTestId(resultsTestId);
+
+    /* print the count of buttons on the page */
+    console.info("number of checkbox elements:", await checkboxLocator.count());
+    
+    /* usage of count() method */
+    let numberOfFoundCheckboxes = await checkboxLocator.count();
+    for (let i = 0; i < numberOfFoundCheckboxes; i++) {
+      await checkboxLocator.nth(i).click();
+      /* display the text content of the results element */
+      console.info("results text content:", await resultsLocator.textContent());
+    }
+
+    /* Assert: */
+    await expect(resultsLocator).toHaveText(expectedMessage1);
+
+    /* usage of all() method */
+    for (const checkbox of await checkboxLocator.all()) {
+      await checkbox.click();
+      console.info("results text content:", await resultsLocator.textContent());
+    }
+
+    /* Assert: */
+    await expect(resultsLocator).toHaveText(expectedMessage2);
+  });
+});
