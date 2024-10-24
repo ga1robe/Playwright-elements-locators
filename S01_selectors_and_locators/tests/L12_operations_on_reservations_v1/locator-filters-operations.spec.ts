e8fd5571bf8a8c8be10528f0f7a1873e90eb9147
Robert Gajewski <robert.gajewski@gmail.com>
2024-10-24 21:46:13 +0200

    L12-locator-filters-operation - solution to the task

--- S01_selectors_and_locators/tests/L12_operations_on_reservations_v1/locator-filters-operations.spec.ts
+++ S01_selectors_and_locators/tests/L12_operations_on_reservations_v1/locator-filters-operations.spec.ts
@@ -10,17 +10,21 @@ test.describe("Locator filters", () => {
   test.describe("Finding element - reservation v1 approaches", () => {
     test("Single button click using options", async ({ page }) => {
       // Arrange:
-      const elementRole = "checkbox";
+      const checkboxElementRole = "checkbox";
       const btnElementRole = "button";
-      const reserveButtonText = "Reserve";
-      const checkoutButtonText = "Checkout";
-      const expectedMessage = "Reservation for 23.10.2024 with features: Food for total price: 150$";
+      const reserveBtnText = "Reserve";
+      const checkoutBtnText = "Checkout";
+      const featureName = "Food";
+      const reservationDate = "23.10.2024";
+      const expectedMessage = "Reservation for "+reservationDate+" with features: "+featureName+" for total price: 150$";
       const resultsTestId = "dti-results";
 
       // we can define the locators for the elements
-      const checkboxLocator = page.locator('tr').nth(1).locator('td').nth(3).getByRole(elementRole);
-      const reserveButtonLocator = page.getByRole(btnElementRole).getByText(reserveButtonText).nth(1);
-      const checkoutButtonLocator = page.getByRole(btnElementRole).getByText(checkoutButtonText);
+      // const checkboxLocator = page.locator('tr').nth(1).locator('td').nth(3).getByRole(checkboxElementRole);
+      const checkboxLocator = page.getByRole("row", {name: featureName}).getByRole(checkboxElementRole);
+      // const reserveButtonLocator = page.getByRole(btnElementRole).getByText(reserveBtnText).nth(1);
+      const reserveButtonLocator = page.getByRole("row", {name: reservationDate}).getByRole(btnElementRole, {name: reserveBtnText});
+      const checkoutButtonLocator = page.getByRole(btnElementRole, {name: checkoutBtnText});
 
       const resultsLocator = page.getByTestId(resultsTestId);
 
